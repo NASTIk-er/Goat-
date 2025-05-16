c11fcf7b878fe0bb9de8e2a7e0622c3d8711951e
NASTIk-er <balermeta@gmail.com>
2025-05-16 09:58:27 +0600

    Add files via upload

--- scripts/cmds/4k.js
+++ scripts/cmds/4k.js
@@ -6,7 +6,7 @@ module.exports = {
     aliases: ["upscale"],
     version: "1.1",
     role: 0,
-    author: "saidul",
+    author: "Team Calyx",
     countDown: 5,
     longDescription: "Upscale images to 4K resolution.",
     category: "image",
